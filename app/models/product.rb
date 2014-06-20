893f5d7ab128187f36c275736576b1fee39f794d
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-20 16:55:03 -0400

    disp prices in dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
 
+	 def formatted_price
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("%.2f", price_in_dollars)
+  end
+
 validates :description, :name, :presence => true
 validates :price_in_cents, :numericality => [:only_integer => true]
 end
